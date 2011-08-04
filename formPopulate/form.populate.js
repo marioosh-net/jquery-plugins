63b45738d94e70b2f816dfb343a4f1bc1049aeaf
marioosh <marioosh.net@gmail.com>
2011-08-04 11:49:13 +0200

    populate plugin: examples

--- formPopulate/form.populate.js
+++ formPopulate/form.populate.js
@@ -1,17 +1,21 @@
 /**
  * populate children inputs identified by attribute attr with JSON
  * 
- * example:
- * 		$('#id_form').myPopulate(json, 'name');
- * 
- * json: { "name" : "Jack", "id" : "12", "description" : "description..." }
- * form:
+ * Examples:
+ *      json: { "name" : "Jack", "id" : "12", "description" : "description..." }
  * 		<form>
  * 			<input type="text" name="name" />
  * 			<input type="text" name="id" />
  * 			<textarea type="text" name="description" />
  * 		</form>
+ * 		$('#form1').myPopulate(json, 'name');
  * 		
+ * 		<form id="form2">
+ * 			<input type="text" name="nick" alt="name" />
+ * 			<input type="text" name="identifier" alt="id" />
+ * 			<textarea type="text" name="desc" alt="description" />
+ * 		</form>
+ * 		$('#form2').myPopulate(json, 'alt');
  */
 (function($) {
 	$.fn.myPopulate = function(json, attr) {
